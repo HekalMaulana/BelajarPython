33e4624b5b93418146357c72e920f1a74c5bfb7b
HekalMaulana <116002257+HekalMaulana@users.noreply.github.com>
2022-10-22 14:52:24 +0800

    Latihan Dictionary
    Pelatihan Pembuatan Program Data Mahasiswa Dengan Dictionary Di Python

--- main.py
+++ main.py
@@ -1,29 +1,61 @@
-# Latihan List (Program List Buuku)
+# Latihan Dictionary (Data Mahasiswa)
+import datetime 
+import os
+import string
+import random
 
-from operator import index
+# FromKeys
 
+# Template Dict Mahasiswa
+mahasiswaTemp = {
+    'nama':'nama',
+    'nim':'00000000',
+    'sks':0,
+    'lahir':datetime.datetime(1111,1,11),    
+}
+# Template Dict Mahasiswa End
 
-listBuku = []
+dataMahasiswa = {}
+
 while True:
-    print("\nProgram List Data Buku")
-    print("="*50)
 
-    judul = input("\n\nMasukan Judul Buku\t: ")
-    penulis = input("Masukan Penulis Buku\t: ")
+    # CLear Screen Pada Terminal
+    os.system("cls")
+    # Clear Screen End
 
-    dataBuku = [judul,penulis]
-    listBuku.append(dataBuku)
+    print(f"{'Selamat Datang':^20}")
+    print(f"{'Data Mahasiswa':^20}")
+    print("="*20)
 
-    print("\n\nList Data Buku")
-    print("="*50)
+    mahasiswa = dict.fromkeys(mahasiswaTemp.keys())
+    # print(mahasiswa)
+    mahasiswa['nama'] = input("Nama Mahasiswa : ")
+    mahasiswa['nim'] = input("Nim Mahasiswa : ")
+    mahasiswa['sks'] = int(input("SKS Lulus Mahasiswa : "))
+    TAHUN = int(input("Tahun Lahir (YYYY) : "))
+    BULAN = int(input("Bulan Lahir (1-12) : "))
+    TANGGAL = int(input("Tanggal Lahir (1-31) : "))
+    mahasiswa ['lahir'] = datetime.datetime(TAHUN,BULAN,TANGGAL) 
 
-    for index,buku in enumerate(listBuku):
-        print(f"{index+1} | Judul = {buku[0]} | Penulis = {buku[1]}")
+    KEY = ''.join((random.choice(string.ascii_uppercase)for  i in range (6)))
+    dataMahasiswa.update({KEY:mahasiswa})
 
-    print("\n\n","="*50)
-    done = input("Apakah Anda Ingin Melanjutkan (y/n) : ")
+    print(f"\n{'KEY':<6} {'NAMA' :<17} {'NIM' :<10} {'SKS' :^10} {'LAHIR' :^10} ")
+    print("="*60) 
+    for mahasiswa in dataMahasiswa:
+        KEY = mahasiswa
 
-    if done == "n":
-        break;
+        NAMA = dataMahasiswa[KEY]['nama']
+        NIM = dataMahasiswa[KEY]['nim']
+        SKS = dataMahasiswa[KEY]['sks']
+        LAHIR = dataMahasiswa[KEY]['lahir'].strftime("%x")
 
-print("\n\nPROGRAM SELESAI")
+        print(f"{KEY:<6} {NAMA :<17} {NIM :<10} {SKS :^10} {LAHIR :^10} ") 
+
+    print("\n")
+    done = input("Apakah Ingin Melanjutkan (y/n) : ")
+
+    if done == 'n':
+        break
+
+print("PROGRAM SELESAI")
